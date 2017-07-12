d743c0dae64235b9cea57b30a69eca479c14d59a
padarshr <padarshr@codeaurora.org>
2017-07-12 16:46:43 +0530

    trident: Update vndk-sp build mechanism and library set.
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.
    
    Change-Id: Iac86c8ece08c2dae94fce6e94deb670aa742e3dc

--- device.mk
+++ device.mk
@@ -72,22 +72,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# VNDK
+# VNDK-SP
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0.vndk-sp \
-    android.hardware.graphics.allocator@2.0.vndk-sp \
-    android.hardware.graphics.mapper@2.0.vndk-sp \
-    android.hardware.graphics.common@1.0.vndk-sp \
-    android.hidl.base@1.0.vndk-sp \
-    libhwbinder.vndk-sp \
-    libbase.vndk-sp \
-    libcutils.vndk-sp \
-    libhardware.vndk-sp \
-    libhidlbase.vndk-sp \
-    libhidltransport.vndk-sp \
-    libutils.vndk-sp \
-    libc++.vndk-sp \
-    libsync.vndk-sp \
-    libbacktrace.vndk-sp \
-    libunwind.vndk-sp \
-    liblzma.vndk-sp
+    vndk-sp
