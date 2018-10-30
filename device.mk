32c1b06ac82999c34e52720c36cceacd1962d91d
1nfrag <sil3ntcll@gmail.com>
2018-10-30 21:18:02 +0800

    trident: Build NoCutoutOverlay
    Change-Id: I94d80335f58217d0af91273ad1ae476f69ba07a9

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
