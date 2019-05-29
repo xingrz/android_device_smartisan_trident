5266b8fc8442c86db1e8dde601f30accdd1fa99a
XiNGRZ <chenxingyu92@gmail.com>
2019-05-29 20:31:06 +0800

    trident: Uprev SoundTrigger 2.1
    Change-Id: I92517a854a9c9f616da1004625ca6d0c4917123a

--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
