2a04f69610564160025193a1279ef9018abac153
XiNGRZ <chenxingyu92@gmail.com>
2019-08-01 10:54:59 +0800

    trident: twrp: Include crypto
    Change-Id: Ica3c104a73697728b6b04a465775ab56090a2324

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_IGNORE_MISC_WIPE_DATA := true
+TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
