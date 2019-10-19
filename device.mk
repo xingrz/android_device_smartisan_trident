b508a177ce77b3c71fbeedbff2ce780a81444586
XiNGRZ <chenxingyu92@gmail.com>
2019-10-19 22:40:38 +0800

    trident: Migrate to new recovery fs layout
    Change-Id: I005aad3bfd36db384b723424d8300281b67da14e

--- device.mk
+++ device.mk
@@ -314,6 +314,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,$(TARGET_COPY_OUT_RECOVERY)/root)
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
